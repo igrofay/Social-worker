fea77fbca22b84cef1abb9aae4672d04d503cceb
СИТ <itismymessagebox@mail.ru>
2021-07-04 17:41:16 +0500

    1.0.4

--- app/src/main/java/com/example/socialworker/MainActivity.java
+++ app/src/main/java/com/example/socialworker/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.socialworker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.profileFragment, R.id.plannedSchedulsFragment, R.id.activeWorkFragment)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+
     }
 
 
